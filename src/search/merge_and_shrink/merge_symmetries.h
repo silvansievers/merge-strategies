68d16aedb4a757aa2790ce442a81b42778d8f373
Silvan Sievers <silvan.sievers@unibas.ch>
2014-06-06 09:55:46 +0200

    [ms-symetries] Backed out changeset: 0599808022d9

--- src/search/merge_and_shrink/merge_symmetries.h
+++ src/search/merge_and_shrink/merge_symmetries.h
@@ -1,13 +1,13 @@
 #ifndef MERGE_AND_SHRINK_MERGE_SYMMETRIES_H
 #define MERGE_AND_SHRINK_MERGE_SYMMETRIES_H
 
-#include "merge_linear.h"
+#include "merge_dfp.h"
 
 #include "../option_parser.h"
 
 #include <set>
 
-class MergeSymmetries : public MergeLinear {
+class MergeSymmetries : public MergeDFP {
     const Options options;
     std::set<int> abs_to_merge;
     bool started_merging_for_symmetries;
