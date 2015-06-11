523025c3754e1eb2403c01433a452e084a9326aa
Silvan Sievers <silvan.sievers@unibas.ch>
2015-06-11 11:23:25 +0300

    [merge-sccs] simplify options to be either linear or dfp (no combinations for internal/external thereof)

--- src/search/merge_and_shrink/merge_sccs.cc
+++ src/search/merge_and_shrink/merge_sccs.cc
@@ -23,10 +23,8 @@ bool compare_sccs_decreasing(const set<int> &lhs, const set<int> &rhs) {
 
 MergeSCCs::MergeSCCs(const Options &options)
     : MergeDFP(),
-      internal_scc_merge_order(InternalSCCMergeOrder(options.get_enum("internal_scc_merge_order"))),
-      composite_scc_merge_order(CompositeSCCMergeOrder(options.get_enum("composite_scc_merge_order"))),
-      number_of_merges_for_scc(0),
-      merged_sccs(false) {
+      merge_order(MergeOrder(options.get_enum("merge_order"))),
+      number_of_merges_for_scc(0) {
 
     // First step: compute SCCs and put them in the right order.
     vector<vector<int> > cg;
@@ -40,7 +38,6 @@ MergeSCCs::MergeSCCs(const Options &options)
     if (sccs.size() == 1) {
         cout << "found single scc, continue as regular merge with the "
                 "chosen external merge setting" << endl;
-        merged_sccs = true;
     } else {
         cout << "found cg sccs:" << endl;
         for (size_t i = 0; i < sccs.size(); ++i) {
@@ -73,53 +70,81 @@ MergeSCCs::MergeSCCs(const Options &options)
             break;
         }
 
-        current_transition_systems.reserve(g_variable_domain.size() * 2 - 1);
-        // Second step: if the internal scc merge order is linear, compute the order.
-        if (internal_scc_merge_order == INTERNAL_LINEAR) {
-            VariableOrderFinder order(VariableOrderType(options.get_enum("variable_order")));
-            vector<int> variable_order;
-            variable_order.reserve(g_variable_domain.size());
-            while (!order.done()) {
-                variable_order.push_back(order.next());
-            }
-            if (internal_scc_merge_order == INTERNAL_LINEAR) {
-                internal_linear_order.reserve(variable_order.size());
-                int next_ts_index = g_variable_domain.size();
-                while (!cg_sccs.empty()) {
-                    set<int> &scc = cg_sccs.back();
-                    int first = -1;
-                    int second = -1;
-                    for (size_t j = 0; j < variable_order.size(); ++j) {
-                        int var = variable_order[j];
-                        if (scc.count(var)) {
-                            if (first == -1) {
-                                first = var;
-                            } else if (second == -1) {
-                                second = var;
-                                internal_linear_order.push_back(make_pair(first, second));
-                                ++next_ts_index;
-                            } else {
-                                internal_linear_order.push_back(make_pair(next_ts_index, var));
-                                ++next_ts_index;
-                            }
-                            scc.erase(var);
-                        }
-                    }
-                    cg_sccs.erase(cg_sccs.end());
-                }
-                cout << "precomputed internal scc merge order: ";
-                for (size_t i = 0; i < internal_linear_order.size(); ++i) {
-                    cout << internal_linear_order[i].first << ", "
-                         << internal_linear_order[i].second << endl;
-                }
-            }
-        } else if (internal_scc_merge_order == INTERNAL_DFP) {
+        if (merge_order == DFP) {
             current_transition_systems.reserve(g_variable_domain.size());
         }
     }
+
+    // Second step: if the merge order is linear, compute the order.
+    if (merge_order == LINEAR) {
+        VariableOrderFinder order(VariableOrderType(options.get_enum("variable_order")));
+        vector<int> variable_order;
+        variable_order.reserve(g_variable_domain.size());
+        while (!order.done()) {
+            variable_order.push_back(order.next());
+        }
+        linear_order.reserve(g_variable_domain.size() - 1);
+        int next_ts_index = g_variable_domain.size();
+        unordered_map<int, int> var_to_ts_index;
+        // compute the merge order within sccs.
+        while (!cg_sccs.empty()) {
+            set<int> &scc = cg_sccs.back();
+            int first = -1;
+            int second = -1;
+            int ts_index_after_scc = next_ts_index + scc.size() - 1;
+            for (size_t j = 0; j < variable_order.size(); ++j) {
+                int var = variable_order[j];
+                if (scc.count(var)) {
+                    if (first == -1) {
+                        first = var;
+                    } else if (second == -1) {
+                        second = var;
+                        linear_order.push_back(make_pair(first, second));
+                        ++next_ts_index;
+                    } else {
+                        linear_order.push_back(make_pair(next_ts_index, var));
+                        ++next_ts_index;
+                    }
+                    scc.erase(var);
+                    var_to_ts_index[var] = ts_index_after_scc;
+                    if (scc.empty()) {
+                        assert(ts_index_after_scc == next_ts_index);
+                        break;
+                    }
+                }
+            }
+            cg_sccs.erase(cg_sccs.end());
+        }
+        cout << "precomputed internal scc merge order: ";
+        for (size_t i = 0; i < linear_order.size(); ++i) {
+            cout << linear_order[i].first << ", "
+                 << linear_order[i].second << endl;
+        }
+
+        // compute linear merge order after having merged the sccs.
+        int first = variable_order[0];
+        if (var_to_ts_index.count(first)) {
+            first = var_to_ts_index[first];
+        }
+        int second = variable_order[1];
+        if (var_to_ts_index.count(second)) {
+            second = var_to_ts_index[second];
+        }
+        linear_order.push_back(make_pair(first, second));
+        ++next_ts_index;
+        for (size_t i = 2; i < variable_order.size(); ++i) {
+            int var = variable_order[i];
+            if (var_to_ts_index.count(var)) {
+                var = var_to_ts_index[var];
+            }
+            linear_order.push_back(make_pair(next_ts_index, var));
+            ++next_ts_index;
+        }
+        assert(linear_order.size() == g_variable_domain.size() - 1);
+    }
 }
 
-pair<int, int> MergeSCCs::get_next_internal_dfp() {
+pair<int, int> MergeSCCs::get_next_dfp() {
     set<int> &current_scc = cg_sccs.back();
     pair<int, int> next_pair = MergeDFP::get_next(current_transition_systems);
     /*
@@ -138,126 +163,89 @@ pair<int, int> MergeSCCs::get_next_internal_dfp() {
 pair<int, int> MergeSCCs::get_next(const std::vector<TransitionSystem *> &all_transition_systems) {
     assert(!done());
 
-    if (!merged_sccs) {
-        if (internal_scc_merge_order == INTERNAL_LINEAR) {
-            // use next precomputed pair
-            pair<int, int> next_pair = internal_linear_order.front();
-            internal_linear_order.erase(internal_linear_order.begin());
-            --remaining_merges;
-            if (internal_linear_order.empty()) {
-                merged_sccs = true;
-            }
-            return next_pair;
-        } else if (internal_scc_merge_order == INTERNAL_DFP) {
-            if (!number_of_merges_for_scc && !cg_sccs.empty()) {
-                set<int> &current_scc = cg_sccs.back();
-                assert(current_scc.size() > 1);
-                assert(current_transition_systems.empty());
+    if (merge_order == LINEAR) {
+        // use next precomputed pair
+        pair<int, int> next_pair = linear_order.front();
+        linear_order.erase(linear_order.begin());
+        --remaining_merges;
+        return next_pair;
+    } else if (merge_order == DFP) {
+        if (!number_of_merges_for_scc && !cg_sccs.empty()) {
+            set<int> &current_scc = cg_sccs.back();
+            assert(current_scc.size() > 1);
+            assert(current_transition_systems.empty());
 
-                number_of_merges_for_scc = current_scc.size() - 1;
-                if (number_of_merges_for_scc == 1) {
-                    --remaining_merges;
-                    --number_of_merges_for_scc;
-                    assert(!number_of_merges_for_scc);
-                    int first = *current_scc.begin();
-                    int second = *(++current_scc.begin());
-                    cg_sccs.erase(cg_sccs.end());
-                    return make_pair(first, second);
-                }
-
-                // Initialize current transition systems with all those contained in the scc
-                for (size_t i = 0; i < all_transition_systems.size(); ++i) {
-                    if (current_scc.count(i)) {
-                        TransitionSystem *ts = all_transition_systems[i];
-                        assert(ts);
-                        current_transition_systems.push_back(ts);
-                    } else {
-                        current_transition_systems.push_back(0);
-                    }
-                }
-                return get_next_internal_dfp();
-            }
-
-            if (number_of_merges_for_scc > 1) {
-                // Add the newest transition system to the set of current ones of the scc
-                current_transition_systems.push_back(all_transition_systems.back());
-                return get_next_internal_dfp();
-            }
-
+            number_of_merges_for_scc = current_scc.size() - 1;
             if (number_of_merges_for_scc == 1) {
-                current_transition_systems.push_back(all_transition_systems.back());
-                pair<int, int> next_pair;
-                bool looking_for_first = true;
-                for (size_t i = 0; i < current_transition_systems.size(); ++i) {
-                    if (current_transition_systems[i]) {
-                        if (looking_for_first) {
-                            next_pair.first = i;
-                            looking_for_first = false;
-                        } else {
-                            next_pair.second = i;
-                            break;
-                        }
-                    }
-                }
-                cout << "Next pair of indices: (" << next_pair.first << ", " << next_pair.second << ")" << endl;
-
-                // Assert that we merged all transition systems that we expected to merge.
-                set<int> &current_scc = cg_sccs.back();
-                current_scc.erase(next_pair.first);
-                current_scc.erase(next_pair.second);
-                assert(current_scc.empty());
-                current_transition_systems[next_pair.first] = 0;
-                current_transition_systems[next_pair.second] = 0;
-                for (size_t i = 0; i < current_transition_systems.size(); ++i) {
-                    assert(!current_transition_systems[i]);
-                }
-                current_transition_systems.clear();
-                cg_sccs.erase(cg_sccs.end());
-                if (cg_sccs.empty()) {
-                    merged_sccs = true;
-                }
-
                 --remaining_merges;
                 --number_of_merges_for_scc;
                 assert(!number_of_merges_for_scc);
-                return next_pair;
+                int first = *current_scc.begin();
+                int second = *(++current_scc.begin());
+                cg_sccs.erase(cg_sccs.end());
+                return make_pair(first, second);
             }
-        }
-    }
 
-    if (merged_sccs) {
-        if (composite_scc_merge_order == LINEAR_CREATION_ORDER) {
-            int first = -1;
+            // Initialize current transition systems with all those contained in the scc
             for (size_t i = 0; i < all_transition_systems.size(); ++i) {
-                if (all_transition_systems[i]) {
-                    if (first == -1) {
-                        first = i;
-                    } else {
-                        --remaining_merges;
-                        return make_pair(first, i);
-                    }
+                if (current_scc.count(i)) {
+                    TransitionSystem *ts = all_transition_systems[i];
+                    assert(ts);
+                    current_transition_systems.push_back(ts);
+                } else {
+                    current_transition_systems.push_back(0);
                 }
             }
-        } else if (composite_scc_merge_order == REVERSE_LINEAR_CREATION_ORDER) {
-            int first = -1;
-            for (int i = static_cast<int>(all_transition_systems.size() - 1);
-                    i >= 0; --i) {
-                if (all_transition_systems[i]) {
-                    if (first == -1) {
-                        first = i;
-                    } else {
-                        --remaining_merges;
-                        return make_pair(first, i);
-                    }
-                }
-            }
-        } else if (composite_scc_merge_order == COMPOSITE_DFP) {
-            MergeDFP::get_next(all_transition_systems);
+            return get_next_dfp();
         }
-    }
 
-    ABORT("MergeSCCs failure");
-    return make_pair(-1, -1);
+        if (number_of_merges_for_scc > 1) {
+            // Add the newest transition system to the set of current ones of the scc
+            current_transition_systems.push_back(all_transition_systems.back());
+            return get_next_dfp();
+        }
+
+        if (number_of_merges_for_scc == 1) {
+            current_transition_systems.push_back(all_transition_systems.back());
+            pair<int, int> next_pair;
+            bool looking_for_first = true;
+            for (size_t i = 0; i < current_transition_systems.size(); ++i) {
+                if (current_transition_systems[i]) {
+                    if (looking_for_first) {
+                        next_pair.first = i;
+                        looking_for_first = false;
+                    } else {
+                        next_pair.second = i;
+                        break;
+                    }
+                }
+            }
+            cout << "Next pair of indices: (" << next_pair.first << ", " << next_pair.second << ")" << endl;
+
+            // Assert that we merged all transition systems that we expected to merge.
+            set<int> &current_scc = cg_sccs.back();
+            current_scc.erase(next_pair.first);
+            current_scc.erase(next_pair.second);
+            assert(current_scc.empty());
+            current_transition_systems[next_pair.first] = 0;
+            current_transition_systems[next_pair.second] = 0;
+            for (size_t i = 0; i < current_transition_systems.size(); ++i) {
+                assert(!current_transition_systems[i]);
+            }
+            current_transition_systems.clear();
+            cg_sccs.erase(cg_sccs.end());
+
+            --remaining_merges;
+            --number_of_merges_for_scc;
+            assert(!number_of_merges_for_scc);
+            return next_pair;
+        }
+
+        return MergeDFP::get_next(all_transition_systems);
+    } else {
+        ABORT("Unknown merge order");
+        return make_pair(-1, -1);
+    }
 }
 
 string MergeSCCs::name() const {
@@ -274,13 +262,13 @@ static MergeStrategy *_parse(OptionParser &parser) {
                            orders,
                            "choose an ordering of the sccs",
                            "topological");
-    vector<string> internal_scc_merge_order;
-    internal_scc_merge_order.push_back("linear");
-    internal_scc_merge_order.push_back("dfp");
-    parser.add_enum_option("internal_scc_merge_order",
-                           internal_scc_merge_order,
-                           "choose a merge order to be applied within sccs: "
-                           "linear or dfp. if linear, provide option.",
+    vector<string> merge_order;
+    merge_order.push_back("linear");
+    merge_order.push_back("dfp");
+    parser.add_enum_option("merge_order",
+                           merge_order,
+                           "choose a merge order: linear (specify "
+                           "variable_order)  or dfp.",
                            "dfp");
     vector<string> variable_order;
     variable_order.push_back("CG_GOAL_LEVEL");
@@ -291,18 +279,9 @@ static MergeStrategy *_parse(OptionParser &parser) {
     variable_order.push_back("REVERSE_LEVEL");
     parser.add_enum_option("variable_order",
                            variable_order,
+                           "option useful if merge_order = linear. "
                            "see VariableOrderFinder",
                            "reverse_level");
-    vector<string> composite_scc_merge_order;
-    composite_scc_merge_order.push_back("linear_creation_order");
-    composite_scc_merge_order.push_back("linear_reverse_creation_order");
-    composite_scc_merge_order.push_back("composite_dfp");
-    parser.add_enum_option("composite_scc_merge_order",
-                           composite_scc_merge_order,
-                           "choose a merge order to be applied after merging "
-                           "every scc: linear in the order in which the sccs "
-                           "have been merged or the reversed order, or dfp.",
-                           "composite_dfp");
     Options options = parser.parse();
     if (parser.dry_run())
         return 0;
--- src/search/merge_and_shrink/merge_sccs.h
+++ src/search/merge_and_shrink/merge_sccs.h
@@ -14,28 +14,20 @@ class MergeSCCs : public MergeDFP {
         DECREASING,
         INCREASING
     };
-    enum InternalSCCMergeOrder {
-        INTERNAL_LINEAR,
-        INTERNAL_DFP
+    enum MergeOrder {
+        LINEAR,
+        DFP
     };
-    InternalSCCMergeOrder internal_scc_merge_order;
-    enum CompositeSCCMergeOrder {
-        LINEAR_CREATION_ORDER,
-        REVERSE_LINEAR_CREATION_ORDER,
-        COMPOSITE_DFP
-    };
-    CompositeSCCMergeOrder composite_scc_merge_order;
+    MergeOrder merge_order;
 
     // TODO: use unordered_set?
     // cg_sccs contain the sccs in order to be merged, from last to first.
     std::vector<std::set<int> > cg_sccs;
     int number_of_merges_for_scc;
     std::vector<TransitionSystem *> current_transition_systems;
-    bool merged_sccs;
+    std::vector<std::pair<int, int> > linear_order;
 
-    std::vector<std::pair<int, int> > internal_linear_order;
-
-    std::pair<int, int> get_next_internal_dfp();
+    std::pair<int, int> get_next_dfp();
 protected:
     virtual void dump_strategy_specific_options() const {}
 public:
