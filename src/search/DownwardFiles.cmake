99e9c2b5c35204b67d7b789f109b1a45eb686bfd
Silvan Sievers <silvan.sievers@unibas.ch>
2015-10-29 18:48:20 +0100

    [merge-strategies] fix previous merge: add branch files to CMake files

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -260,7 +260,9 @@ fast_downward_plugin(
         merge_and_shrink/labels.cc
         merge_and_shrink/merge_and_shrink_heuristic.cc
         merge_and_shrink/merge_dfp.cc
+        merge_and_shrink/merge_dynamic_weighted.cc
         merge_and_shrink/merge_linear.cc
+        merge_and_shrink/merge_sccs.cc
         merge_and_shrink/merge_strategy.cc
         merge_and_shrink/shrink_bisimulation.cc
         merge_and_shrink/shrink_bucket_based.cc
@@ -268,6 +270,7 @@ fast_downward_plugin(
         merge_and_shrink/shrink_random.cc
         merge_and_shrink/shrink_strategy.cc
         merge_and_shrink/transition_system.cc
+        scc.cc
 )
 
 fast_downward_plugin(
