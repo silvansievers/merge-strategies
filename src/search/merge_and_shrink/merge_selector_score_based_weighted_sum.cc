6b17a29694dbe2bc097a2b630931fe5e4c77733b
Silvan Sievers <silvan.sievers@unibas.ch>
2021-03-19 15:17:17 +0100

    fix score-based weighted sum selector: require and use total_order scoring for tie-breaking

--- src/search/merge_and_shrink/merge_selector_score_based_weighted_sum.cc
+++ src/search/merge_and_shrink/merge_selector_score_based_weighted_sum.cc
@@ -7,6 +7,7 @@
 #include "../options/options.h"
 #include "../options/plugin.h"
 
+#include "../utils/hash.h"
 #include "../utils/logging.h"
 
 using namespace std;
@@ -41,6 +42,13 @@ MergeSelectorScoreBasedWeightedSum::MergeSelectorScoreBasedWeightedSum(
         cerr << "Normalizing scores currently doesn't work." << endl;
         utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
     }
+    if (merge_scoring_functions.back()->get_name() != "total order") {
+        cerr << "This merge selector requires using the total order scoring "
+                "function as the last in the list for tie-breaking in cases "
+                "where two or more merge candidates have the same best "
+                "weighted sum as score." << endl;
+        utils::exit_with(utils::ExitCode::SEARCH_INPUT_ERROR);
+    }
 }
 
 void MergeSelectorScoreBasedWeightedSum::initialize(
@@ -56,7 +64,8 @@ pair<int, int> MergeSelectorScoreBasedWeightedSum::select_merge(
     const vector<int> &indices_subset) const {
     vector<pair<int, int>> merge_candidates =
         compute_merge_candidates(fts, indices_subset);
-    int num_functions = merge_scoring_functions.size();
+    // The last scoring function has to be "total order" for tie-breaking.
+    int num_functions = merge_scoring_functions.size() - 1;
     int num_merge_candidates = merge_candidates.size();
 
     // Compute all raw score values
@@ -68,7 +77,15 @@ pair<int, int> MergeSelectorScoreBasedWeightedSum::select_merge(
         scores[function_index] = scoring_function->compute_scores(
             fts, merge_candidates);
     }
-//    cout << "raw scores " << scores << endl;
+//    cout << "raw scores" << endl;
+//    for (int cand_ind = 0; cand_ind < num_merge_candidates; ++cand_ind) {
+//        cout << "candidate " << merge_candidates[cand_ind].first
+//             << ", " << merge_candidates[cand_ind].second << ": ";
+//        for (int function = 0; function < num_functions; ++function) {
+//             cout << scores[function][cand_ind] << ", ";
+//        }
+//        cout << endl;
+//    }
 
     // If desired, normalize all scores.
     if (normalize) {
@@ -99,11 +116,19 @@ pair<int, int> MergeSelectorScoreBasedWeightedSum::select_merge(
         }
         scores.swap(normalized_scores);
     }
-//    cout << "normalized scores " << scores << endl;
+//    cout << "normalized scores" << endl;
+//    for (int cand_ind = 0; cand_ind < num_merge_candidates; ++cand_ind) {
+//        cout << "candidate " << merge_candidates[cand_ind].first
+//             << ", " << merge_candidates[cand_ind].second << ": ";
+//        for (int function = 0; function < num_functions; ++function) {
+//            cout << scores[function][cand_ind] << ", ";
+//        }
+//        cout << endl;
+//    }
 
     // Compute the weighted sum over all (normalized) scores.
-    vector<double> weighted_sums;
-    weighted_sums.reserve(num_merge_candidates);
+    vector<double> weighted_summed_scores;
+    weighted_summed_scores.reserve(num_merge_candidates);
     for (int merge_index = 0; merge_index < num_merge_candidates;
          ++merge_index) {
         double weighted_sum = 0;
@@ -131,18 +156,27 @@ pair<int, int> MergeSelectorScoreBasedWeightedSum::select_merge(
                 weighted_sum = INF;
             }
         }
-        weighted_sums.push_back(weighted_sum);
+        weighted_summed_scores.push_back(weighted_sum);
     }
-//    cout << "weighted sums: " << weighted_sums << endl;
+//    cout << "weighted summed scores" << endl;
+//    for (int cand_ind = 0; cand_ind < num_merge_candidates; ++cand_ind) {
+//        cout << "candidate " << merge_candidates[cand_ind].first
+//             << ", " << merge_candidates[cand_ind].second << ": "
+//             << weighted_summed_scores[cand_ind] << endl;
+//    }
 
     // Find the lowest scoring merge candidate.
-    int best_merge_index = -1;
     double best_score = INF;
+    vector<int> best_merge_indices;
     for (int merge_index = 0; merge_index < num_merge_candidates;
          ++merge_index) {
-        if (weighted_sums[merge_index] < best_score) {
-            best_score = weighted_sums[merge_index];
-            best_merge_index = merge_index;
+        int score = weighted_summed_scores[merge_index];
+        if (score < best_score) {
+            best_score = score;
+            best_merge_indices.clear();
+            best_merge_indices.push_back(merge_index);
+        } else if (score == best_score) {
+            best_merge_indices.push_back(merge_index);
         }
     }
     if (best_score == INF) {
@@ -152,17 +186,22 @@ pair<int, int> MergeSelectorScoreBasedWeightedSum::select_merge(
         utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
     }
 
-    int best_score_counter = 0;
-    for (int merge_index = 0; merge_index < num_merge_candidates;
-         ++merge_index) {
-        if (weighted_sums[merge_index] == best_score) {
-            ++best_score_counter;
-        }
-        if (best_score_counter > 1) {
-            cerr << "At least two merge candidates have the exact same best "
-                    "score! Did you forget to include a randomizing scoring "
-                    "function for tie-breaking?" << endl;
-            utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
+    int best_merge_index = -1;
+    if (best_merge_indices.size() == 1) {
+        best_merge_index = best_merge_indices.front();
+    } else {
+        const shared_ptr<MergeScoringFunction> &total_order_scoring_function =
+            merge_scoring_functions.back();
+        vector<double> tie_breaking_scores =
+            total_order_scoring_function->compute_scores(fts, merge_candidates);
+        double best_score = INF;
+        for (int merge_index : best_merge_indices) {
+            int score = tie_breaking_scores[merge_index];
+            assert(score != best_score);
+            if (score < best_score) {
+                best_score = score;
+                best_merge_index = merge_index;
+            }
         }
     }
 
