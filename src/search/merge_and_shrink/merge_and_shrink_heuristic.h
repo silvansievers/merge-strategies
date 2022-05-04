8598a272a2e84855d3ed530ee386f2a7f81ae811
Silvan Sievers <silvan.sievers@unibas.ch>
2022-05-04 10:41:15 +0200

    remove partial_mas option, which was lastly implemented around 2018

--- src/search/merge_and_shrink/merge_and_shrink_heuristic.h
+++ src/search/merge_and_shrink/merge_and_shrink_heuristic.h
@@ -11,26 +11,14 @@ enum class Verbosity;
 
 namespace merge_and_shrink {
 class FactoredTransitionSystem;
-class FactorScoringFunction;
 class MergeAndShrinkRepresentation;
-
-enum class PartialMASMethod {
-    None,
-    Single,
-    Maximum
-};
 
 class MergeAndShrinkHeuristic : public Heuristic {
     const utils::Verbosity verbosity;
 
-    // Options related to computing partial abstractions
-    const PartialMASMethod partial_mas_method;
-    std::vector<std::shared_ptr<FactorScoringFunction>> factor_scoring_functions;
-
     // The final merge-and-shrink representations, storing goal distances.
     std::vector<std::unique_ptr<MergeAndShrinkRepresentation>> mas_representations;
 
-    int find_best_factor(const FactoredTransitionSystem &fts) const;
     void extract_factor(FactoredTransitionSystem &fts, int index);
     bool extract_unsolvable_factor(FactoredTransitionSystem &fts);
     void extract_nontrivial_factors(FactoredTransitionSystem &fts);
